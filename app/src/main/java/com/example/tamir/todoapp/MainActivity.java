b37efde8deee9bf5c1f06b3fcb3d850fad9a9e45
Tamir91 <1hotlev@gmail.com>
2017-07-29 18:09:05 +0300

    v_0.2

--- app/src/main/java/com/example/tamir/todoapp/MainActivity.java
+++ app/src/main/java/com/example/tamir/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tamir.todoapp;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,8 +20,10 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonSave, buttonUndo, buttonSettings;
+    Button buttonUndo, buttonSettings, buttonSetTitle;
+    ImageButton buttonSave;
     EditText editText;
+    TextView textViewTitle;
     ListView listView;
     ArrayAdapter arrayAdapter;
     ArrayList<String> arrayListString = new ArrayList<>();
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        getData();
         setListeners();
     }
 
@@ -39,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 insertDataToList(editText.getText().toString());
+            }
+        });
+
+        buttonSetTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setData("title", editText.getText().toString());
+                textViewTitle.setText(editText.getText().toString());
             }
         });
 
@@ -56,11 +69,13 @@ public class MainActivity extends AppCompatActivity {
                 if (!arrayRecycleBin.isEmpty()) {
                     insertDataToList(arrayRecycleBin.get(arrayRecycleBin.size() - 1));
                     arrayRecycleBin.remove(arrayRecycleBin.size() - 1);
-                } else
-                    Toast.makeText(context, "The stack is empty", Toast.LENGTH_SHORT).show();
+                }
+                if (arrayRecycleBin.isEmpty())
+                    buttonUndo.setVisibility(View.GONE);
             }
         });
-/*Click button for delete*/
+
+        /*Click on list view for delete*/
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(final AdapterView<?> adapterView, final View view, int i, long l) {
@@ -110,11 +125,25 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Todo was added", Toast.LENGTH_SHORT).show();
     }
 
+    public void getData() {
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        textViewTitle.setText(settings.getString("title", "title"));
+    }
+
+    private void setData(String key, String value) {
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
     public void initViews() {
-        buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave = (ImageButton) findViewById(R.id.buttonSave);
         buttonUndo = (Button) findViewById(R.id.buttonUndo);
         buttonSettings = (Button) findViewById(R.id.buttonSettings);
+        buttonSetTitle = (Button) findViewById(R.id.buttonTitle);
         editText = (EditText) findViewById(R.id.editText);
+        textViewTitle = (TextView) findViewById(R.id.textViewTitle);
         listView = (ListView) findViewById(R.id.listView);
     }
 }
