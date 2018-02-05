77a72afade98838a784c52f0ce3d9838a9f893e1
Mazderatti <Michael1984sh@gmail.com>
2018-02-05 21:58:17 +0200

    comm

--- app/src/main/java/com/example/tamir/todoapp/MainActivity.java
+++ app/src/main/java/com/example/tamir/todoapp/MainActivity.java
@@ -23,7 +23,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.UUID;
 
-// Hi I'm Mazderatti!!
+// Hi I'm Mazderatti!!!
 public class MainActivity extends AppCompatActivity {
     ImageButton buttonAdd;
     ListView listView;
